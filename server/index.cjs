a5f536622c1574f6fe592458465d7dd2fc701559
Louisaflor <louisa.yonzon6@gmail.com>
2022-11-20 21:27:31 -0800

    was able to connect the server I created to the front end.. LETS GO

--- server/index.cjs
+++ server/index.cjs
@@ -4,13 +4,27 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 
+const server = http.createServer(app);
+
+// const io = require("socket.io")(server, {
+//   cors: {
+//     origin: "http://localhost:5173",
+//     methods: ["GET", "POST"],
+//     credentials: true,
+//   },
+// });
+
+/////////////////
+
 app.use(cors()); //Add CORS middleware
 
-const server = http.createServer(app);
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 const io = new Server(server, {
   cors: {
-    origin: "http://127.0.0.1:5173/",
+    origin: "http://localhost:5173",
     method: ["GET", "POST"],
   },
 });
@@ -20,6 +34,9 @@ io.on("connection", (socket) => {
   console.log(`User connected ${socket.id}`);
 
   //In here is where we write our socket event listeners in here
+  socket.on("send_message", (msg) => {
+    console.log("here is the message in the server: ", msg);
+  });
 });
 
 server.listen(4000, () =>
--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,27 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import io from 'socket.io-client'
 
-const socket = io.connect('http://locahost:4000') //Adding this, out server for the socket will run on port 4000
+//////////
+//import vitePluginSocketIO from "vite-plugin-socket-io";
+
+const socket = io.connect('http://127.0.0.1:4000') //Adding this, out server for the socket will run on port 4000
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState()
+
+  const sendMesage = (e) => {
+    console.log('sending the message here: ', message)
+    socket.emit('send_message' , {message})
+    setMessage("")
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <input type="text" placeholder="Message..." value={message} onChange={(e) => setMessage(e.target.value)} />
+      <button onClick={sendMesage}>Submit</button>
     </div>
   )
 }
